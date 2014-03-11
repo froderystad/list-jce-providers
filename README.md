075a693354758c92129b613895cac8e5740077ad
Frode Rystad <frode-git@rystad.no>
2014-03-11 18:16:32 +0100

    Refactored all features into one servlet. Recovered from mistakenly commiting tabs instead of spaces ;)

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ A simple servlet that lists JCA providers installed on your application server.
 
 Created in the context of this discussion on the WildFly forum: https://community.jboss.org/message/861688
 
-When deployed to JBoss or WildFly, go to http://localhost:8080/jce/ListJceProviders and http://localhost:8080/jce/JceStatus (adjust for your port offset).
+When deployed to JBoss or WildFly, go to http://localhost:8080/jce/status (adjust for your port offset).
--- src/main/java/no/jpro/examples/jceproviders/JceStatusServlet.java
+++ src/main/java/no/jpro/examples/jceproviders/JceStatusServlet.java
@@ -1,14 +1,6 @@
 package no.jpro.examples.jceproviders;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.Provider;
-import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -19,66 +11,79 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.security.*;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
-@WebServlet(urlPatterns = "/JceStatus")
+@WebServlet(urlPatterns = "/status")
 public class JceStatusServlet extends HttpServlet {
 
-	private static final Charset UTF8 = Charset.forName("UTF-8");
-	private static final String ASYMMETRIC_KEY_ALG = "RSA";
-	private static final String ASYMMETRIC_CIPHER_ALG = "RSA/ECB/PKCS1Padding";
-	private static final String BOUNCY_CASTLE_NAME = "BC";
-	private static final String MY_SECRET = "My secret";
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+    private static final String ASYMMETRIC_KEY_ALG = "RSA";
+    private static final String ASYMMETRIC_CIPHER_ALG = "RSA/ECB/PKCS1Padding";
+    private static final String BOUNCY_CASTLE_NAME = "BC";
+    private static final String MY_SECRET = "My secret";
 
-	@Override
-	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter writer = response.getWriter();
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
 
-		printWhetherBouncyCastleIsAvailable(writer);
-		printEncryptionReport(writer);
-		printWhetherBouncyCastleCanBeInstantiated(writer);
-	}
+        printWhetherBouncyCastleIsAvailable(writer);
+        printEncryptionReport(writer);
+        printWhetherBouncyCastleCanBeInstantiated(writer);
+        writer.println();
+        printRegisteredJceProviders(writer);
+    }
 
-	private void printWhetherBouncyCastleIsAvailable(final PrintWriter writer) {
-		Provider bcProvider = Security.getProvider(BOUNCY_CASTLE_NAME);
-		writer.println("BouncyCastle (BC) is available: " + (bcProvider != null));
-	}
+    private void printWhetherBouncyCastleIsAvailable(final PrintWriter writer) {
+        Provider bcProvider = Security.getProvider(BOUNCY_CASTLE_NAME);
+        writer.println("BouncyCastle (BC) is available: " + (bcProvider != null));
+    }
 
-	@SuppressWarnings({ "unused", "MismatchedReadAndWriteOfArray" })
-	private void printEncryptionReport(final PrintWriter writer) throws ServletException {
-		try {
-			KeyPair keyPair = generateKeyPair();
-			Cipher cipher = Cipher.getInstance(ASYMMETRIC_CIPHER_ALG);
+    @SuppressWarnings({"unused", "MismatchedReadAndWriteOfArray"})
+    private void printEncryptionReport(final PrintWriter writer) throws ServletException {
+        try {
+            KeyPair keyPair = generateKeyPair();
+            Cipher cipher = Cipher.getInstance(ASYMMETRIC_CIPHER_ALG);
 
-			cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
-			byte[] encryptedBytes = cipher.doFinal(MY_SECRET.getBytes(UTF8));
+            cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPrivate());
+            byte[] encryptedBytes = cipher.doFinal(MY_SECRET.getBytes(UTF8));
 
-			cipher.init(Cipher.DECRYPT_MODE, keyPair.getPublic());
-			byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-			String decryptedString = new String(decryptedBytes, UTF8);
+            cipher.init(Cipher.DECRYPT_MODE, keyPair.getPublic());
+            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+            String decryptedString = new String(decryptedBytes, UTF8);
 
-			writer.println("Successfully encrypted/decrypted using RSA with provider: " + cipher.getProvider().getName());
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
-			throw new ServletException(e);
-		}
-	}
+            writer.println("Successfully encrypted/decrypted using RSA with provider: " + cipher.getProvider().getName());
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
+            throw new ServletException(e);
+        }
+    }
 
-	private KeyPair generateKeyPair() throws NoSuchAlgorithmException {
-		KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ASYMMETRIC_KEY_ALG);
-		return keyGen.genKeyPair();
-	}
+    private KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ASYMMETRIC_KEY_ALG);
+        return keyGen.genKeyPair();
+    }
 
-	private void printWhetherBouncyCastleCanBeInstantiated(final PrintWriter writer) {
-		writer.println("Can instantiate BouncyCastleProvider: " + canInstantiateBouncyCastleProvider());
-	}
+    private void printWhetherBouncyCastleCanBeInstantiated(final PrintWriter writer) {
+        writer.println("Can instantiate BouncyCastleProvider: " + canInstantiateBouncyCastleProvider());
+    }
 
-	private boolean canInstantiateBouncyCastleProvider() {
-		try {
-			new BouncyCastleProvider();
-			return true;
-		} catch (Throwable e) {
-			return false;
-		}
-	}
+    private boolean canInstantiateBouncyCastleProvider() {
+        try {
+            new BouncyCastleProvider();
+            return true;
+        } catch (Throwable e) {
+            return false;
+        }
+    }
+
+    private void printRegisteredJceProviders(PrintWriter writer) {
+        writer.println("Registered JCE providers:");
+
+        int index = 1;
+        for (Provider provider : Security.getProviders()) {
+            writer.println(index++ + ": " + provider.toString());
+        }
+    }
 }
