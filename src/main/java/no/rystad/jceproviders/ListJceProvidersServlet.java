f80ab488dadd196cc25290b47aa1c9ac9263bf12
Frode Rystad <frode-git@rystad.no>
2014-03-11 07:04:50 +0100

    Initial commit of servlet and POM.

---
+++ src/main/java/no/rystad/jceproviders/ListJceProvidersServlet.java
@@ -0,0 +1,23 @@
+package no.rystad.jceproviders;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Provider;
+import java.security.Security;
+
+@WebServlet(urlPatterns = "/ListJceProviders")
+public class ListJceProvidersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+
+        for (Provider provider : Security.getProviders()) {
+            writer.println(provider.toString());
+        }
+    }
+}
